1c95f292075394aea94872eab79bf8fc685c6a6a
hasnolife <kovalenko.richman@gmail.com>
2023-01-16 17:19:38 +0200

    Create Splash screen and logo

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:online_store/widgets/my_app_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const orange = Color(0xFFff6e4e);
+  static const dark = Color(0xFF010035);
+}
